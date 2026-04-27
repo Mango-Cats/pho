4a9007b3f33bcb56e92d305c7571e57b7320912b
zrygan <zr.gntn@gmail.com>
2026-04-28 05:18:36 +0800

    Added an idea for the algorithms handling

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -1,2 +1,32 @@
 pub mod aline;
 pub mod editex;
+
+pub enum SimilarityAlgorithm {
+    Aline,
+    Editex,
+}
+
+pub enum SimilarityAlgorithmConfig {
+    AlineConfig,
+    EditexConfig,
+}
+
+#[warn(dead_code)]
+impl SimilarityAlgorithm {
+    /// This is he shared similarity
+    fn similarity(
+        self,
+        x: &'static str,
+        y: &'static str,
+        // this is option since some algorithms have a config (or a
+        // set of variables) while others don't.
+        similarity_config: Option<&SimilarityAlgorithmConfig>,
+    ) -> f32 {
+        match self {
+            SimilarityAlgorithm::Aline => todo!(),
+            SimilarityAlgorithm::Editex => todo!(),
+            _ => panic!("`SimilarityAlgorithm` doesn't cover all possible cases"),
+        }
+        10.1
+    }
+}
