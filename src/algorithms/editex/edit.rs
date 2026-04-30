c69c5d1326265082abaad5d4958c931c2a81a64b
zrygan <zr.gntn@gmail.com>
2026-04-30 13:06:36 +0800

    Added edit operations

--- src/algorithms/editex/edit.rs
+++ src/algorithms/editex/edit.rs
@@ -1,7 +1,25 @@
-pub fn replace() {
-    todo!();
+use super::{config::EditexConfig, group::same_group};
+
+pub fn replace(a: char, b: char, config: &EditexConfig) -> f32 {
+    if a == b {
+        return 0.0;
+    }
+
+    if same_group(a, b, config) {
+        config.costs.same
+    } else {
+        config.costs.diff
+    }
 }
 
-pub fn delete() {
-    todo!()
+pub fn delete(current: char, previous: Option<char>, config: &EditexConfig) -> f32 {
+    let Some(previous) = previous else {
+        return config.costs.diff;
+    };
+
+    if same_group(current, previous, config) {
+        config.costs.same
+    } else {
+        config.costs.diff
+    }
 }
