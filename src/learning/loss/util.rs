2b3a6785e6d33620fd8b8b44178f53df1c279b0b
zrygan <zr.gntn@gmail.com>
2026-05-05 16:05:52 +0800

    Fix

--- src/learning/loss/util.rs
+++ src/learning/loss/util.rs
@@ -2,7 +2,13 @@
 /// given the precomputed base algorithm scores and the current GA weights.
 #[inline]
 pub(super) fn predict(base_scores: &[f32], weights: &[f32]) -> f32 {
-    base_scores
+    let aligned_scores = if base_scores.len() == weights.len() + 1 {
+        &base_scores[1..]
+    } else {
+        base_scores
+    };
+
+    aligned_scores
         .iter()
         .zip(weights.iter())
         .map(|(score, weight)| score * weight)
